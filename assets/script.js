893ccfd32ad33b34d2701d816af3b52ad2ce2495
zoe <zoeedge0703@gmail.com>
2023-09-16 20:09:10 -0500

    revamped the index.html and adding the new query selectors in script.js

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,8 @@
 var apiKey = '675793050fa3144fb1fed403f292b937'
 
-var searchArea = document.querySelector('.search-area');
+var searchArea = document.querySelector('#search-area');
+var searchInput = document.querySelector('#search-input')
+var cardTitle = document.querySelector('.card-title')
 var temp = document.querySelector('#temp');
 var wind = document.querySelector('#wind');
 var humidity = document.querySelector('#humidity');
