fbc8cae94a2b918ecda1f3dfd65c37472660814f
zoe <zoeedge0703@gmail.com>
2023-09-16 20:25:08 -0500

    revamped the script.js to match the additions in the index and also fixed a few issues within the script

--- assets/script.js
+++ assets/script.js
@@ -6,14 +6,12 @@ var cardTitle = document.querySelector('.card-title')
 var temp = document.querySelector('#temp');
 var wind = document.querySelector('#wind');
 var humidity = document.querySelector('#humidity');
-var cityDate = document.querySelector('#cityDate');
-var currentDate = dayjs().format('MM/DD/YYYY');
 var fiveDayEl = document.querySelector('#forecast');
-var weatherContainer = document.querySelector('#weather-container');
+var currentDay = document.querySelector('.current-day')
+var currentDate = document.querySelector('MM/DD/YYYY')
+
 
 function getGeo (city) {
-    
-    // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
     fetch(`https://api.openweathermap.org/geo/1.0/direct?q=${city}&appid=${apiKey}`)
     .then(function (response) {
         return response.json();
@@ -30,7 +28,8 @@ function getWeather(lat, lon, city) {
         return response.json();
     })
     .then(function (data) {
-        temp.innerHTML = 'Temperature: ' + Math.floor(data.main.temp) + ' F';
+        cardTitle.innerHTML = city + ' ' + '(' + currentDate + ')';
+        temp.innerHTML = 'Temperature: ' + Math.floor(data.main.temp) + 'F';
         humidity.innerHTML = 'Humidity: ' + Math.floor(data.main.humidity) + '%';
         cityDate.innerHTML = city + ' ' + '(' + currentDate + ')';
         wind.innerHTML = 'wind: ' + Math.floor(data.wind.speed) + ' mph';
@@ -51,16 +50,16 @@ function getForecast(lat, lon) {
                 fiveDayEl.appendChild(containerEl);
                 var cardEl = document.createElement('div');
                 cardEl.setAttribute('class', 'card');
+                cardEl.setAttribute('id', 'forecast-card')
                 cardEl.setAttribute('style', 'margin: 10px 10px 10px 10px; width: 10rem;')
                 containerEl.appendChild(cardEl);
                 var cardBodyEl = document.createElement('div')
                 cardBodyEl.setAttribute('class', 'card-body');
                 cardEl.appendChild(cardBodyEl);
-                var dataEl = document.createElement('h5');
-                dataEl.innerHTML = dayjs((data.list[i]).dt_txt.substring(0, 10).format('MM/DD/YYYY'));
+                var dateEl = document.createElement('h5');
+                dateEl.innerHTML = dayjs((data.list[i]).dt_txt.substring(0, 10).format('MM/DD/YYYY'));
                 dateEl.setAttribute('class', 'card-title text-dark');
                 dateEl.setAttribute('style', 'text-align: center;');
-                var iconUrl = 'https://openweathermap.org/img/w' + data.list[i].weather[0].icon + '.png';
                 var iconEl = document.createElement('h6');
                 iconEl.innerHTML = '<img src="https://openweathermap.org/img/w" '+ data.list[i].weather[0].icon +' ".png" alt= "weather icon">';
                 iconEl.setAttribute('class', 'card-title text-dark');
@@ -85,6 +84,14 @@ function getForecast(lat, lon) {
             }
         }
     })
+}
+
+function clearFiveDay() {
+    fiveDayEl.innerHTML = '';
+}
+
+function setView(view) {
+    currentDay.style.display = view === 'START' ? null  : 'none';
 }
 
 getGeo();
